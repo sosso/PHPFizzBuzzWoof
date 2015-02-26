61fc0529eccaf185f5b4b3e0b5d317ffbeb2dc94
Anthony Sosso <asosso@findthebest.com>
2015-02-25 18:57:11 -0800

    [SWAP AFTER] Impl: if 5, return buzz.  if 3 or 6, return fizz

--- src/FizzBuzzComputer.php
+++ src/FizzBuzzComputer.php
@@ -3,8 +3,11 @@
 class FizzBuzzComputer {
 
     public function generate($argument1) {
-        if ($argument1 === 3) {
+        if ($argument1 === 3 || $argument1 === 6) {
 			return 'fizz';
+		}
+        if ($argument1 === 5) {
+			return 'buzz';
 		}
 		return "$argument1";
     }
