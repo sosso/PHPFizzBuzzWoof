ec659f5199189b48ce61268ac3eaa3566572ce98
Anthony Sosso <asosso@findthebest.com>
2015-02-25 19:02:59 -0800

    Impl: just fizz if %3 instead and new rule 7 === woof

--- src/FizzBuzzComputer.php
+++ src/FizzBuzzComputer.php
@@ -3,11 +3,14 @@
 class FizzBuzzComputer {
 
     public function generate($argument1) {
-        if ($argument1 === 3 || $argument1 === 6) {
+        if ($argument1 % 3 === 0) {
 			return 'fizz';
 		}
         if ($argument1 === 5) {
 			return 'buzz';
+		}
+		if ($argument1 === 7) {
+			return 'woof';
 		}
 		return "$argument1";
     }
