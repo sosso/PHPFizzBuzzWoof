bcbb7970db0ab829fdf279c03848dce6af8dd03c
Anthony Sosso <asosso@findthebest.com>
2015-02-25 18:49:46 -0800

    Test: created helper function AND 3->fizz

--- spec/FizzBuzzComputerSpec.php
+++ spec/FizzBuzzComputerSpec.php
@@ -7,10 +7,19 @@ use Prophecy\Argument;
 
 class FizzBuzzComputerSpec extends ObjectBehavior {
 	function it_returns_1_for_1() {
-		$this->generate(1)->shouldReturn('1');
+		$this->callGenerate(1, '1');
 	}
 
 	function it_returns_2_for_2() {
-		$this->generate(2)->shouldReturn('2');
+		$this->callGenerate(2, '2');
+	}
+
+	function it_returns_fizz_for_3() {
+		$this->callGenerate(3, 'fizz');
+	}
+
+
+	private function callGenerate($in, $expected) {
+		$this->generate($in)->shouldReturn($expected);
 	}
 }
