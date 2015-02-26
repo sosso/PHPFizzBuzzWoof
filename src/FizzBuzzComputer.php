a04b4edb5e9b2d1865027a20c727346e20987c1a
Anthony Sosso <asosso@findthebest.com>
2015-02-25 19:07:24 -0800

    Impl: just use mod for rules and we're good

--- src/FizzBuzzComputer.php
+++ src/FizzBuzzComputer.php
@@ -6,10 +6,10 @@ class FizzBuzzComputer {
         if ($argument1 % 3 === 0) {
 			return 'fizz';
 		}
-        if ($argument1 === 5) {
+        if ($argument1 % 5 === 0) {
 			return 'buzz';
 		}
-		if ($argument1 === 7) {
+		if ($argument1 % 7 === 0) {
 			return 'woof';
 		}
 		return "$argument1";
